a847821e3ece12d65b6c4aa10ca746c046401494
Mehdi_SOULA <mehdi.soula-external@generali-invest.com>
2020-01-31 19:47:04 +0100

    Vidly - Route + Form
    Route  implentation + Login Form

--- vidly/src/Starter Code/Resources/Section 6- Routing/start/vidly/src/App.js
+++ vidly/src/Starter Code/Resources/Section 6- Routing/start/vidly/src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import Customers from "./components/common/customers";
+import Rentals from "./components/common/rentals";
+import NotFound from "./components/common/notfound";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <main className="container">
+          <NavBar />
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
